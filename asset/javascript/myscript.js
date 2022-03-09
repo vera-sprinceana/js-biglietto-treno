3b9fdabe59fc138f1b44b38efbd6683bba85a72a
vera.sprinceana <sprinceana.vera96@gmail.com>
2022-03-09 18:29:46 +0100

    prezzo finale in forma umana

--- asset/javascript/myscript.js
+++ asset/javascript/myscript.js
@@ -9,23 +9,23 @@ console .log (età);
 document.getElementById("età").innerHTML=età;
 //calcolo del prezzo in base ai km (0.21 € al km)
 prezzoAlKm=(0.21)
-var prezzo=(chilometri) * (prezzoAlKm).toFixed(4);
+var prezzo=(chilometri) * (prezzoAlKm).toFixed(2);
 console .log (prezzo); 
-document.getElementById("prezzo").innerHTML=prezzo;
+document.getElementById("prezzo").innerHTML=prezzo.toFixed(2);
 //sconto del 20% per i minorenni va applicato uno sconto del 40% per gli over 65
-sconto1=(20)
-let prezzoScontato1 = ((prezzo / 100).toFixed(2) * (20))- (prezzo) ;
-sconto2=(40)
-let prezzoScontato2 = ((prezzo / 100).toFixed(2) * (40)) - (prezzo);
+sconto1=("20")
+let prezzoScontato1 = ((prezzo / 100) * (sconto1))- (prezzo).toFixed(2);
+sconto2=("40")
+let prezzoScontato2 = ((prezzo / 100) * (sconto2)) - (prezzo).toFixed(2);
 
 if(età < 18){
     prezzoScontato1
     console .log (prezzoScontato1);
-    document.getElementById("prezzoScontato1").innerHTML=prezzoScontato1;
+    document.getElementById("prezzoScontato1").innerHTML=prezzoScontato1.toFixed(2);
 }else if (età => 65){
     prezzoScontato2
     console .log (prezzoScontato2);
-    document.getElementById("prezzoScontato2").innerHTML=prezzoScontato2;
+    document.getElementById("prezzoScontato2").innerHTML=prezzoScontato2.toFixed(2);
 }else (età > 18);{
     prezzo
 }
